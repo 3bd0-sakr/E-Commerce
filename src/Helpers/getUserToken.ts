ebacef6444f7868fc40b8b839d998f6ad529f303
3bd0-sakr <body85557@gmail.com>
2025-12-30 16:17:28 +0200

    new update

--- src/Helpers/getUserToken.ts
+++ src/Helpers/getUserToken.ts
@@ -3,7 +3,7 @@ import { decode } from "next-auth/jwt"
 import { cookies } from "next/headers"
 
 export async function getUserToken() {
-    const x = (await cookies()).get("next-auth.session-token")?.value || (await cookies()).get("__Secure-next-auth.session-token")?.value
-    const accessToken = await decode({ token: x, secret: process.env.AUTH_SECRET! })
-    return accessToken?.token
+    const x = (await cookies()).get("next-auth.session-token")?.value ||(await cookies()).get("__Secure-next-auth.session-token")?.value
+    const accesstoken = await decode({ token: x, secret: process.env.NEXTAUTH_SECRET! })
+    return accesstoken?.token
 }
